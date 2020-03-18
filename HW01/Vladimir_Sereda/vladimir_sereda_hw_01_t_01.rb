3c416e83d0f1fa67e958cac82682ad3e63c7565f
Vladimir <vovkati3@gmail.com>
2020-03-18 14:49:25 +0300

    homework number one

---
+++ HW01/Vladimir_Sereda/vladimir_sereda_hw_01_t_01.rb
@@ -0,0 +1,21 @@
+puts 'Number of hourse'
+hours = gets.to_i
+
+puts 'Number of minutes'
+minutes = gets.to_i
+
+def angel(hours, minutes)
+# Cloc hand position
+  a = ((hours * 60) + minutes) * 0.5
+# Minute hand position
+  b = minutes * 6
+# finding the angel between them
+  c = a - b
+  if c.abs > 180
+    return 360 - c.abs
+  else
+    return c.abs
+  end
+end
+
+puts "#{angel(hours,minutes)}"
---
+++ HW01/Vladimir_Sereda/vladimir_sereda_hw_01_t_02.rb
@@ -0,0 +1,3 @@
+def number
+  24 == 1/3 + 4 * 6
+end
