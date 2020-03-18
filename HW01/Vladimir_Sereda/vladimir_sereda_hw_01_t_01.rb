52073608d5e568c36ff3baad5a79f1fba38a4322
Vladimir <vovkati3@gmail.com>
2020-03-18 16:55:40 +0300

    variable declaration changes

--- HW01/Vladimir_Sereda/vladimir_sereda_hw_01_t_01.rb
+++ HW01/Vladimir_Sereda/vladimir_sereda_hw_01_t_01.rb
@@ -4,15 +4,15 @@ hours = gets.to_i
 puts 'Number of minutes'
 minutes = gets.to_i
 
-def angel(hours, minutes)
+def angle(hours, minutes)
   cloc_hand_position = ((hours * 60) + minutes) * 0.5
   minute_hand_position = minutes * 6
-  angel_between_them = cloc_hand_position - minute_hand_position
-  if angel_between_them.abs > 180
-    360 - angel_between_them.abs
+  angle_between_cloc_and_minute_hand = cloc_hand_position - minute_hand_position
+  if angle_between_cloc_and_minute_hand.abs > 180
+    360 - angle_between_cloc_and_minute_hand.abs
   else
-    angel_between_them.abs
+    angle_between_cloc_and_minute_hand.abs
   end
 end
 
-puts angel(hours, minutes).to_s
+puts angle(hours, minutes)
