e97d19e26e066e38c7233e3a232407e4749d0c99
Vladimir <vovkati3@gmail.com>
2020-03-27 19:56:58 +0300

    error correction with the condition

--- HW02/Vladimir_Sereda/vladimir_sereda_hw_02_t_01.rb
+++ HW02/Vladimir_Sereda/vladimir_sereda_hw_02_t_01.rb
@@ -3,7 +3,7 @@
 logs = File.readlines('logs/logs_ts1.txt', chomp: true)
 
 def error_message(logs)
-  logs.select { |keyword| keyword.downcase =~ /.*error.*/ }
+  logs.select { |keyword| keyword.downcase =~ /.*error.*/ }.first
 end
 
 puts error_message(logs)
