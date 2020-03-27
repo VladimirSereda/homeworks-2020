29562c4fe29bf26fc736495009bcbda2a7f7acce
Vladimir <vovkati3@gmail.com>
2020-03-27 19:33:47 +0300

    correction of errors in the task1

--- HW02/Vladimir_Sereda/vladimir_sereda_hw_02_t_01.rb
+++ HW02/Vladimir_Sereda/vladimir_sereda_hw_02_t_01.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-logs = File.readlines('logs/logs_ts1.txt', chomp: true )
+logs = File.readlines('logs/logs_ts1.txt', chomp: true)
 
 def error_message(logs)
   logs.select { |keyword| keyword.downcase =~ /.*error.*/ }
