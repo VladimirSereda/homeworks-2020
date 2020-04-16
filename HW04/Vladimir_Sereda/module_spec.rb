5558f009b7da1980e9dd4b8025d783ebbb96af65
Vladimir <vovkati3@gmail.com>
2020-04-16 14:36:27 +0300

    correction errors

--- HW04/Vladimir_Sereda/module_spec.rb
+++ HW04/Vladimir_Sereda/module_spec.rb
@@ -7,7 +7,7 @@ describe Array do
 
     context 'when given array [1,2,3,4,5]' do
       it 'return values from array [1,2,3,4,5]' do
-        expect do |_elem|
+        expect do
           subject.for_each { |elem| puts elem }
         end.to output("1\n2\n3\n4\n5\n").to_stdout
       end
@@ -19,24 +19,20 @@ describe Array do
 
     context 'when given array [1,2,3,4,5]' do
       it 'return  [2,4,6,8,10]' do
-        expect do |_elem|
-          result = subject.for_map { |elem| elem * 2 }
-          expect(result).to match_array [2, 4, 6, 8, 10]
-        end
+        expect(subject.for_map { |elem| elem * 2 }).to
+        match_array [2, 4, 6, 8, 10]
       end
     end
   end
+end
 
-  describe '#for_select' do
-    subject { described_class.new([1, 2, 3, 4, 5]) }
+describe '#for_select' do
+  subject { described_class.new([1, 2, 3, 4, 5]) }
 
-    context 'when given array [1,2,3,4,5]' do
-      it 'return an array of numbers [1,2,3]' do
-        expect do |_elem|
-          result = subject.for_select { |elem| elem < 4 }
-          expect(result).to match_array[1, 2, 3]
-        end
-      end
+  context 'when given array [1,2,3,4,5]' do
+    it 'return an array of numbers [1,2,3]' do
+      expect(subject.for_select { |elem| elem < 4 }).to
+      match_array[1, 2, 3]
     end
   end
 end
